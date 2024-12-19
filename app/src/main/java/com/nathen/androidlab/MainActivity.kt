91dedef49fb5fde1ad9705abbafa4da2ae0643c2
Nate <youngmini90@naver.com>
2024-12-19 16:56:52 +0900

    [Add] XML의 뷰를 코드에서 사용해보기 (with id) - id 값으로 뷰 객체 획득

--- app/src/main/java/com/nathen/androidlab/MainActivity.kt
+++ app/src/main/java/com/nathen/androidlab/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.nathen.androidlab
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -8,5 +9,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         // 화면 출력 XML 명시
         setContentView(R.layout.activity_main)
+        // id값으로 뷰 객체 획득
+        val textView1 = findViewById<TextView>(R.id.text1)
     }
 }
